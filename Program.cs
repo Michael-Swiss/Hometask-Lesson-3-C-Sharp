842eb3ab6820ab582c0aca2c7714ff7b33cd55ce
Michael <michael_mosienko@mail.ru>
2023-05-05 01:26:19 +0200

    Added Decision of Homework 3. Task 1.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿// Homework 3. Task 1.
+
+// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+Console.WriteLine("Homework 3. Task 1.");
+Console.Write("Enter number: ");
+string? number = Console.ReadLine();
+
+void CheckingNumber(string number){
+  if (number[0]==number[4] || number[1]==number[3]){
+    Console.WriteLine($"Your number: {number} - is Pallindromme.");
+  }
+  else Console.WriteLine($"Your number: {number} - is NOT Pallindromme.");
+}
+
+if (number!.Length == 5){
+  CheckingNumber(number);
+}
+else Console.WriteLine($"Enter correct number: ");
